341f2f8d4c61c3cb00c1cd2fd6d5dd1af5dfafc1
Prem-its-trap <58662853+Prem-its-trap@users.noreply.github.com>
2021-04-20 23:53:51 +0530

    added first GET route

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [{ name: "john" }, { name: "Sonny" }];
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData,
+  });
+});
+
 app.listen(8000, () => console.log("Server is running"));
