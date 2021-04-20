7e8c27e6e1e7a59ce3ef119d26e5e17300cb7034
Prem-its-trap <58662853+Prem-its-trap@users.noreply.github.com>
2021-04-21 00:03:47 +0530

    added dynamic GET route passes variable in URL

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 
 const mockUserData = [{ name: "john" }, { name: "Sonny" }];
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
   res.json({
     success: true,
     message: "successfully got users. Nice!",
     users: mockUserData,
+  });
+});
+// adding dynamic route using variable in url
+app.get("/user/:id", (req, res) => {
+  console.log(req.params.id);
+  res.json({
+    success: true,
+    message: "Got one user",
+    user: req.params.id,
   });
 });
 
