ac48ba6a921bb5919cf1493ce75a7706e3462674
Prem-its-trap <58662853+Prem-its-trap@users.noreply.github.com>
2021-04-21 00:16:36 +0530

    added post login route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.json());
 
 const mockUserData = [{ name: "john" }, { name: "Sonny" }];
 app.get("/users", (req, res) => {
@@ -9,7 +12,7 @@ app.get("/users", (req, res) => {
     users: mockUserData,
   });
 });
-// adding dynamic route using variable in url
+// adding dynamic route using variable in url colons are used for variabls that be viewd in the params
 app.get("/user/:id", (req, res) => {
   console.log(req.params.id);
   res.json({
@@ -17,6 +20,29 @@ app.get("/user/:id", (req, res) => {
     message: "Got one user",
     user: req.params.id,
   });
+});
+
+// post route
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //   this should come from database
+  const mockUserName = "johndoe";
+  const mockPassword = "superman";
+
+  if (username === mockUserName && password === mockPassword) {
+    res.json({
+      success: true,
+      message: "password and username match",
+      token: "encrption token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and username does not match",
+    });
+  }
 });
 
 app.listen(8000, () => console.log("Server is running"));
